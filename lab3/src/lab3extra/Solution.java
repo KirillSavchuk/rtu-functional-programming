834f3590b66eb89ac34c6cff9fc3626d57462a26
Kirill Savchuk <kirillss1998@gmail.com>
2019-09-18 23:26:58 +0300

    Lab3 EXTRA | powerSum with solution description

--- lab3/src/lab3extra/Solution.java
+++ lab3/src/lab3extra/Solution.java
@@ -11,7 +11,6 @@ public class Solution {
         System.out.print("Enter N:  ");
         Integer N = scanner.nextInt();
         scanner.close();
-        
         System.out.println(powerSum(X, N));  
 	}
 	
@@ -20,7 +19,6 @@ public class Solution {
 	}
 	
 	static long powerSumRecursion(long X, long N, long largest) {
-		System.out.println(X + ", " + N + ", " + largest);
 		long largestPower = (long) Math.pow(largest, N);
 		if (largestPower > X) 
 			return 0;
@@ -29,19 +27,5 @@ public class Solution {
 		else 
 			return powerSumRecursion(X, N, largest+1) + powerSumRecursion(X-largestPower, N, largest+1);
 	}
-	/*  IDEA of powerSumRecursion(10, 2, 1):
-	 	---> [1 step] ---> 1^2=1:
-	 	(10, 2, 1)
-	 	
-	 	---> [2 step] ---> 2^2=4:
-	 	(10, 2, 1) + (10, 2, 2)
-	 	(10, 2, 1) + ( 9, 2, 2) 
-	 	
-	 	---> [3 step] ---> 3^2=9:
-	 	(10, 2, 1) + (10, 2, 2) + (10, 2, 3)
-	 	(10, 2, 1) + (10, 2, 2) + ( 6, 2, 3)
-	 	(10, 2, 1) + ( 9, 2, 2) + ( 9, 2, 3)
-	 	(10, 2, 1) + ( 9, 2, 2) + ( 5, 2, 3) 
-	 */
 
 }
